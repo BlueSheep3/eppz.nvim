c9cf36f3f65408040c35bafb8dec6cd7af3bd3b4
BlueSheep3 <arne.daude@t-online.de>
2025-10-05 15:00:53 +0200

    more adjustments and fixes

--- lua/eppz/groups.lua
+++ lua/eppz/groups.lua
@@ -3,18 +3,19 @@ local M = {}
 local colors = require("eppz.palette")
 
 M.setup = function()
-	return {
+	local G = {
 		-- ==== Standard ====
 
 		-- general
 		Normal = { fg = colors.fg, bg = colors.bg },
+		Visual = { bg = colors.visual },
 		CursorLine = { bg = colors.bglight },
 		CursorLineNr = { fg = colors.cursor_line_nr, bg = colors.bglight },
 		LineNr = { fg = colors.line_nr, bg = colors.bglight },
 
 		-- text
 		Comment = { fg = colors.comment, italic = true },
-		Special = { fg = colors.symbol },
+		Special = { fg = colors.special },
 		Identifier = { fg = colors.variable },
 
 		-- literals
@@ -25,32 +26,64 @@ M.setup = function()
 		-- ==== Treesitter ====
 
 		["@variable"] = { fg = colors.variable },
-		["@variable.builtin"] = { fg = colors.loc },
+		["@variable.builtin"] = { fg = colors.other },
 		["@property"] = { fg = colors.variable },
+
 		["@constant"] = { fg = colors.constant },
 		["@constant.builtin"] = { fg = colors.null },
-		["@module"] = { fg = colors.module },
 		["@string"] = { fg = colors.string },
 		["@character"] = { fg = colors.char },
 		["@boolean"] = { fg = colors.constant },
 		["@number"] = { fg = colors.number },
 		["@number.float"] = { fg = colors.number },
+
 		["@type"] = { fg = colors.type },
 		["@type.builtin"] = { fg = colors.builtin },
-		["@lsp.type.enumMember"] = { fg = colors.type },
+
 		["@attribute"] = { fg = colors.meta },
 		["@attribute.buitlin"] = { fg = colors.meta_dark },
+		["@module"] = { fg = colors.module },
+
 		["@function"] = { fg = colors.func },
 		["@function.builtin"] = { fg = colors.func, bold = true },
 		["@function.macro"] = { fg = colors.func, bold = true },
 		["@lsp.type.macro"] = { fg = colors.func, bold = true },
 		["@constructor"] = { fg = colors.func_other },
-		["@operator"] = { fg = colors.symbol },
+
 		["@keyword"] = { fg = colors.keyword },
+		["@keyword.operator"] = { fg = colors.other },
 		["@keyword.modifier"] = { fg = colors.modifier },
+		["@keyword.directive"] = { fg = colors.meta },
+
 		["@punctuation"] = { fg = colors.symbol },
+		["@punctuation.special"] = { fg = colors.symbol },
+		["@operator"] = { fg = colors.symbol },
 		["@comment"] = { fg = colors.comment },
+
+		-- = Language specific =
+
+		-- enumMembers in rust behave much closer to types
+		-- (other languages will default to @constant)
+		["@lsp.type.enumMember.rust"] = { fg = colors.type },
+
+		-- events in C# for some reason show up as types,
+		-- even though they behave much closer to functions
+		["@lsp.type.event.cs"] = { fg = colors.func, italic = true },
+
+		-- i would want to use constant color for constants,
+		-- but C# doesnt let me differentiate between constans and statics,
+		-- so both of them will get the same color
+		["@lsp.typemod.property.static.cs"] = { fg = colors.loc },
 	}
+
+	-- the newer C# LSPs use the token @lsp.type.keyword.cs for all keywords,
+	-- but i want to be able to differentiate all of the keywords,
+	-- so this will just disable @lsp.type.keyword.cs completely,
+	-- which means the syntactical highlighting will be used instead.
+	vim.api.nvim_set_hl(0, "@lsp.type.keyword.cs", {})
+
+	return G
 end
+
 
 return M
--- lua/eppz/palette.lua
+++ lua/eppz/palette.lua
@@ -2,6 +2,7 @@ return {
 	-- general
 	bg = "#09090b",
 	bglight = "#101018",
+	visual = "#3a3a3a",
 	cursor_line_nr = "#606080",
 	line_nr = "#3f3f44",
 
@@ -34,4 +35,5 @@ return {
 
 	meta = "#5c69a6",
 	meta_dark = "#454e7f",
+	special = "#bb54a9",
 }
